69aea04456012baf9be7df0407609085a7c15450
Jillis ter Hove <j.terhove@gmail.com>
2011-12-16 21:40:28 +0100

    renamed some files

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+import unittest, CoreTest, GMachineTest
+
+
+def load_tests(loader, tests, pattern):
+	test_cases = (CoreTest.CoreTest, GMachineTest.HeapTest, GMachineTest.StackTest)
+	suite = unittest.TestSuite()
+	for test_class in test_cases:
+		tests = loader.loadTestsFromTestCase(test_class)
+		suite.addTests(tests)
+	return suite
